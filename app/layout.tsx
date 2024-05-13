50d9ad6e02cbf6cda9c096d9804bdb5ec0f49eb0
Elías Pintos Aris <diselo.studio@proton.me>
2024-05-13 07:35:29 +0100

    Details

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { getURL } from '@/utils/helpers';
 import 'styles/main.css';
 
 const meta = {
-  title: 'Next.js Subscription Starter',
-  description: 'Brought to you by Vercel, Stripe, and Supabase.',
+  title: 'Slashchurn',
+  description: 'Phase1 Prototype.',
   cardImage: '/og.png',
   robots: 'follow, index',
   favicon: '/favicon.ico',
